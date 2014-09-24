d34795c36bf5794d3a76aede832bfdc596e51a53
katafrakt <inquebrantable@gmail.com>
2014-09-24 21:32:17 +0200

    make sure that id string is valid
    jQuery seems to have some problems with ids containing characters
    like '[', which are common in names generated by rails

--- lib/quill/rails/view_helpers.rb
+++ lib/quill/rails/view_helpers.rb
@@ -11,9 +11,14 @@ module Quill
       def quill_editor(name=nil, options={})
         mod_options = { name: 'quill-value', id: 'quill-value' }.merge options
         @input_name = (name || mod_options[:name])
-        @input_id   = (name || mod_options[:id])   
+        @input_id   = quill_sanitize_id(name || mod_options[:id])
         @value      = mod_options[:value].present? ? mod_options[:value] : ""
         ERB.new(File.read(File.join(source_root, 'template.html.erb'))).result(binding).html_safe
+      end
+
+      private
+      def quill_sanitize_id(id)
+        id.gsub(/\[(.+?)\]/,'_\1')
       end
     end
   end
